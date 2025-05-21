fa2a84d3ad962511f1d7cd920cf64fde83fc296e
DESKTOP-4SRGOIM\surya <surya.s@keprevos.in>
2025-05-21 20:34:17 +0530

    db

--- db.config.js
+++ db.config.js
@@ -1,17 +1,13 @@
+// db.config.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb://localhost:27017/ecom', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            // useCreateIndex: true,
-        });
-
+        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/ecom');
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        process.exit(1); // Exit process with failure
+        process.exit(1);
     }
 };
 
